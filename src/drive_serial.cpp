e80323447812cc0b428cb9b333c2af8e5e0f95e5
Mark Griffith <markygrif12@gmail.com>
2018-01-18 23:37:45 +0000

    Added serial-comm to drive_serial node

--- src/drive_serial.cpp
+++ src/drive_serial.cpp
@@ -2,9 +2,11 @@
 #include <rover1/controller.h>
 #include <rover1/drive_cmd.h>
 #include <string>
+#include <sstream>
 #include "ros/ros.h"
 #include "serial/serial.h"
 #include "std_msgs/Int32.h"
+#include <unistd.h>
 
 class Drive_Serial {
  public:
@@ -27,7 +29,20 @@ class Drive_Serial {
 // Legacy Drive_Serial Object Constructor
 Drive_Serial::Drive_Serial():
   mtude_(0),
-  dir_(0) {
+  dir_(0),
+  port_("/dev/ttyUSB0"),
+  baud_(9600), 
+  my_serial(port_, baud_, serial::Timeout::simpleTimeout(1000)) {
+
+  /* nh_.param("port", port_, port_); */
+  /* nh_.param("baud", baud_, baud_); */
+  std::string is_open = "No";
+  if(my_serial.isOpen())
+    is_open = "Yes";
+  ROS_INFO("Port = %s", port_.c_str());
+  ROS_INFO("Baud = %d", baud_);
+  ROS_INFO("Port Open? [%s]", is_open.c_str());
+
   // Initialize the control_cmd_sub
   control_cmd_sub = nh_.subscribe("/drive_topic", 10,
       &Drive_Serial::centralControlCallback, this);
@@ -42,7 +57,6 @@ Drive_Serial::Drive_Serial(const std::string port_num, uint32_t baud_num):
   baud_(baud_num),
   my_serial(this->port_, (uint32_t)this->baud_,
       serial::Timeout::simpleTimeout(1000)) {
-  
   // Initialize the control_cmd_sub
   control_cmd_sub = nh_.subscribe("/drive_topic", 10,
       &Drive_Serial::centralControlCallback, this);
@@ -57,6 +71,18 @@ void Drive_Serial::centralControlCallback(
   ROS_INFO("[DRIVE] Polar Angle  [%f]", msg->polar_angle);
 
   // TODO(jordan/mark): Forward this data to the Arduino
+  std::stringstream stream;
+  stream << std::fixed << std::setprecision(5) << msg->magnitude;
+  const std::string str = stream.str();
+
+  ROS_INFO("[DRIVE] Sending [%s]", str.c_str());
+
+  uint8_t buf;
+  size_t bytes_sent = this->my_serial.write(str);
+  size_t bytes_read = this->my_serial.read(&buf, str.length());
+
+  ROS_INFO("[DRIVE] Bytes Sent [%zu]", bytes_sent);
+  ROS_INFO("[DRIVE] Read [%s] [%zu bytes]", &buf, bytes_read);
 }
 
 
