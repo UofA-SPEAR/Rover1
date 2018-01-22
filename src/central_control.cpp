81ccde509b49fe1266b87b44e7d07d926657d076
Jordan-Lane <jlane@ualberta.ca>
2018-01-22 17:32:41 +0000

    Added functionality to support both Arduino UNO and MEGA when plugged into serial port

--- src/central_control.cpp
+++ src/central_control.cpp
@@ -10,35 +10,34 @@
 class Central_Control {
  public:
     Central_Control();
-    static void recToPolar(double x, double y, double &r, double &theta);
+    void recToPolar(double x, double y, double &r, double &theta);
 
  private:
-      ros::NodeHandle nh_;
-      ros::Subscriber usercmd_sub;
-      ros::Publisher drive_pub;
-      ros::Publisher arm_pub;
+    ros::NodeHandle nh_;
+    ros::Subscriber usercmd_sub;
+    ros::Publisher drive_pub;
+    ros::Publisher arm_pub;
 
-      void cmdCallback(const rover1::controller::ConstPtr& msg);
+    void cmdCallback(const rover1::controller::ConstPtr& msg);
 };
 
 
 Central_Control::Central_Control() {
-  
-  // Initialize publisher 
+  // Initialize publisher
   drive_pub = nh_.advertise<rover1::drive_cmd>("/drive_topic", 10);
   arm_pub = nh_.advertise<rover1::controller>("/arm_topic", 10);
 
   // Intialize subscriber
-  usercmd_sub = nh_.subscribe("/user_commands", 10, &Central_Control::cmdCallback, this); 
+  usercmd_sub = nh_.subscribe("/user_commands", 10,
+      &Central_Control::cmdCallback, this);
 }
 
 // Command Callback
 void Central_Control::cmdCallback(const rover1::controller::ConstPtr& msg) {
-
-  if(msg->state){
+  // Send the msg to the drive system or arm based on the current state
+  if (msg->state) {
     arm_pub.publish(msg);
-  }
-  else{
+  } else {
     rover1::drive_cmd drive_msg;
     double mtude = 0;
     double angle = 0;
@@ -60,23 +59,21 @@ void Central_Control::recToPolar(double x, double y,
   theta = atan(y/x) * toDegrees;
 
   // Convert theta to the correct quadrant
-  if((x <= 0 && y >= 0) || (x <= 0 && y <= 0)) {
+  if ((x <= 0 && y >= 0) || (x <= 0 && y <= 0)) {
     theta = theta + 180;  // Quad 2 or 3
-  }
-  else if(x >= 0 && y < 0) {
+  } else if (x >= 0 && y < 0) {
     theta = theta + 360;  // Quad 4
   }
-
 }
 
 int main(int argc, char **argv) {
-	//Initializing ROS node with a name of demo_topic_subscriber
-	ros::init(argc, argv,"central_control");
- 
+  // Initializing ROS node with a name of demo_topic_subscriber
+  ros::init(argc, argv, "central_control");
+
   Central_Control control_node;
 
-	//Spinning the node
-	ros::spin();
-	return 0;
+  // Spinning the node
+  ros::spin();
+  return 0;
 }
 
--- src/user_input.cpp
+++ src/user_input.cpp
@@ -17,7 +17,7 @@ class User_Input {
     int x_idx_, y_idx_, tri_idx_;
     double x_scale_, y_scale_;
     // State of the controller - 0 for drive, 1 for arm
-    uint8_t state_; 
+    uint8_t state_;
 
     ros::NodeHandle nh_;
     ros::Publisher controller_cmd_pub;
@@ -32,7 +32,7 @@ User_Input::User_Input():
   y_idx_(1),
   tri_idx_(12),
   x_scale_(-1),
-  y_scale_(1){
+  y_scale_(1) {
   /* nh_.param("axis_linear", linear_, linear_); */
 
   // Initialize publisher
@@ -47,21 +47,26 @@ User_Input::User_Input():
 void User_Input::ps3Callback(const sensor_msgs::Joy::ConstPtr& ps3_msg) {
   rover1::controller msg;
 
-  if(ps3_msg->axes[tri_idx_]){
-    state_ = state_ ^ 1; //XOR
+  if (ps3_msg->buttons[tri_idx_]) {
+    state_ = state_ ^ 1;
     // Delay to keep ROS from reading the button multiple times when pressed
     // Number probs need to be change / finalized
-    usleep(600); 
+    usleep(50000);
   }
 
+  size_t size_axis = sizeof(ps3_msg->axes);
+  size_t size_button = sizeof(ps3_msg->buttons);
   msg.x_coord = x_scale_*ps3_msg->axes[x_idx_];
   msg.y_coord = y_scale_*ps3_msg->axes[y_idx_];
-  
   msg.state = state_;
+
+  //ROS_INFO("LEFT: [%f]", msg.x_coord);
+  //ROS_INFO("RIGHT: [%f]", msg.y_coord);
 
   // Publish the ps3 controller's velocity parameters
   controller_cmd_pub.publish(msg);
 }
+
 
 int main(int argc, char** argv) {
   // Initializing ROS node
